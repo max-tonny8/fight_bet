6dae2c8b689346934c84f8de78240aa81a99ae61
tonny0831 <tonnyjansen0831@gmail.com>
2024-05-24 19:08:17 +0900

    fix: updating connect button

--- src/components/ui/headerBtn/index.styled.ts
+++ src/components/ui/headerBtn/index.styled.ts
@@ -14,6 +14,7 @@ export const Wrapper = styled.div`
   border: solid 1px #cfcfcf;
   box-shadow: 1px 1px 1px 0px #dbdbdb;
   white-space: nowrap;
+  cursor: pointer;
 
   .title {
     display: flex;
--- src/components/ui/headerBtn/index.tsx
+++ src/components/ui/headerBtn/index.tsx
@@ -2,8 +2,8 @@ import { Wrapper } from "./index.styled";
 
 const HeaderBtn = ({ avatar, title, icon, onClick }: HeaderBtnType) => {
   return (
-    <Wrapper>
-      <div className="title" onClick={onClick}>
+    <Wrapper onClick={onClick}>
+      <div className="title">
         {avatar!}
         <p>{title}</p>
       </div>
